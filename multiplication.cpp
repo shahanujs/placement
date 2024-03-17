d72dbe0e3af3eef3aeb51e1a4245a884055b77f1
Disha Patel <disha81100@gmail.com>
2024-03-17 16:18:40 -0700

    Add multiplication file

---
+++ multiplication.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    
+    int num1, num2;
+    
+  
+    std::cout << "Enter the first number: ";
+    std::cin >> num1; 
+    
+   
+    std::cout << "Enter the second number: ";
+    std::cin >> num2; 
+    
+    
+    int product = num1 * num2;
+    
+    std::cout << "Multiplication is " << (num1+num2) << std::endl;
+    
+    return 0;
+}
